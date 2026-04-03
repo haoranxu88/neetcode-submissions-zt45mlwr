867983c61cd3486377d6100fadec44b7bf622327
Haoran Xu <haoran.xu808@gmail.com>
2026-04-02 23:32:29 -0500

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preqs = defaultdict(list)
+        for a, b in prerequisites:
+            preqs[a].append(b)
+        taking = set()
+        taken = set()
+        def dfs(course):
+            if course in taken:
+                return True
+            if course in taking:
+                return False
+            taking.add(course)
+            for p in preqs[course]:
+                if not dfs(p):
+                    return False
+            taking.remove(course)
+            taken.add(course)
+            return True
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
