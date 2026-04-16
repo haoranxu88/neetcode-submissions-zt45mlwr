acde9f25904fbf735d06b1aa8936ee7f0d57c895
Haoran Xu <haoran.xu808@gmail.com>
2026-04-16 07:41:49 -0500

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        res = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
+            grid[r][c] = '0'
+            for dr, dc in dirs:
+                dfs(r + dr, c + dc)
+            return
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    dfs(r, c)
+                    res += 1
+        return res
